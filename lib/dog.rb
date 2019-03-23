303df1ce825e0218d9296483260f1d99f3972323
Sherief Farid <sherief@farid.cloud>
2019-03-23 22:53:38 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+require 'pry'
+
+class Dog
+
+  @@all = []
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << itself
+  end
+
+  def self.all
+    puts @@all
+  end
+
+  def self.clear_all
+    @@all = []
+  end
+end #Endo of class
